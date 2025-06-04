2189a6b0d12aa00973d40349632a61f1960aae7b
Gasper Podergajs <gasper.podergajs@hpe.com>
2025-06-04 09:40:03 +0200

    implemented env file read

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 declare const module: any;
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8000;
   await app.listen(port);
 
+  
+  logger.log(`Listening on port ${port}`);
   // enable HMR - see https://docs.nestjs.com/recipes/hot-reload
   if (module.hot) {
     module.hot.accept();
