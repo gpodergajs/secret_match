156e51197e0277dab91d5a4cb2d5714528a37110
Gasper Podergajs <gasper.podergajs@hpe.com>
2025-06-04 11:59:28 +0200

    added a database model and typeorm support

---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from 'src/users/role.entity';
+import { User } from 'src/users/user.entity';
+
+@Module({
+    imports: [
+        ConfigModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres', // TODO: find a way to add support for multiple driver types if possible
+                host: config.get('DB_HOST'),
+                port: config.get('DB_PORT'),
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_NAME'),
+                entities: [User, Role] // TODO: find a way to list entities dynamically if possible
+            }),
+            inject: [ConfigService]
+        })
+    ],
+    exports: [TypeOrmModule]
+})
+export class DatabaseModule {}
