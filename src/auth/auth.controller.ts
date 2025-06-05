13218bcbf83173d93ce9e3d6d61c99ea06707975
gpodergajs <gpodergajs@gmail.com>
2025-06-05 12:33:14 +0200

    Feature/auth_flow (#4)
    * installed required libraries
    
    * implemented login and jwt authentication ( auth guards )
    
    * modified user controller
    
    ---------
    
    Co-authored-by: Gasper Podergajs <gasper.podergajs@hpe.com>

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+
+@Controller()
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('users/login')
+    async login(@Request() req) {
+        return this.authService.login(req.user)
+    }
+}
--- src/users/dto/login-request.dto.ts
+++ src/users/dto/login-request.dto.ts
@@ -1,4 +1,8 @@
 export class LoginRequestDto {
+    constructor(email: string, password: string) {
+        this.email = email;
+        this.password = password
+    }
     email: string;
     password: string;
 }
